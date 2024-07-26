9bd50b43bb70b538b572aab7cb096afce335b094
Suisha03 <xxxshota2002xxx@gmail.com>
2024-07-26 14:24:27 +0900

    Finished: QUrl_test.py

--- QUrl_test.py
+++ QUrl_test.py
@@ -1,16 +1,26 @@
 from PyQt6.QtCore import QUrl
 
 # URLを作成します。このURLにはパスワードが含まれています。
+"""
 url = QUrl()
+url.setPassword("password")
 url.setScheme("http")
 url.setUserName("username")
-url.setPassword("password")
 url.setHost("www.example.com")
 
-print("Original URL: ", url.toString())
 
+print("Original URL: ", url.toString())
 # QUrl.UrlFormattingOption.RemovePassword フラグを設定します。
-flags = QUrl.UrlFormattingOption.RemovePassword
+flags = QUrl.UrlFormattingOption.RemovePassword | QUrl.ComponentFormattingOption.DecodeReserved 
+
+# フラグを適用してURLを文字列として出力します。
+print("URL without password: ", url.toString(flags))
+"""
+
+url = QUrl("https://username:password@www.example.com/path%20with%20spaces")
+print("Original URL: ", url.toString())
+# QUrl.UrlFormattingOption.RemovePassword フラグを設定します。
+flags = QUrl.UrlFormattingOption.RemovePassword | QUrl.ComponentFormattingOption.DecodeReserved 
 
 # フラグを適用してURLを文字列として出力します。
 print("URL without password: ", url.toString(flags))
